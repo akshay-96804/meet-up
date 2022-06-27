cdf504bc9a39d0c6bb4ef2299270150c12441af9
akshay@96804 <19uec032@lnmiit.ac.in>
2022-06-27 15:53:22 +0530

    Authentication added

--- lib/utils/firebaseHandler.dart
+++ lib/utils/firebaseHandler.dart
@@ -1,55 +1,79 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meet_up/models/eventInfo.dart';
 import 'package:meet_up/models/groupModel.dart';
 
-class FirebaseAdd{
+class FirebaseHelpers{
 
-  addEvent(EventModel event) async{
-    CollectionReference events = FirebaseFirestore.instance.collection('events');
-    await events.doc(event.id).set(event.toJson(event)).whenComplete(() {
-      print("EVENT SUCCESSFULLY ADDED TO FIRESTORE");
-    }).catchError((e){
-      print(e);
-    });
+  FirebaseAuth _firebaseAuth = FirebaseAuth.instance ;
+
+  Future<String> createAccount(String email,String password) async{
+    
+    try {
+      UserCredential _userCred = await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+      // print(_userCred);
+      if(_userCred!=null) return "Success" ;
+
+    } on FirebaseAuthException{
+         
+    } 
+    catch (e) {
+      print(e.toString()) ;
+      return e;
+    }
   }
 
-  addGroup(GroupModel group) async{
-    CollectionReference groups = FirebaseFirestore.instance.collection('groups');
-    await groups.doc(group.title).set(toJSON(group)).whenComplete(() => {
-      print("GROUP ADDED TO FIREBASE SUCCESSFULLY")
-    });
+  Future<String> logInToAccount(String email,String password) async{
+    UserCredential _userCred = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+    if(_userCred!=null) return "Success" ;
   }
 
-  getGroupsNameList(String name){
-    List<String> groupNames = [];
-    CollectionReference groups = FirebaseFirestore.instance.collection("groups");
 
-    var getDocs = groups.get().then((value){
-      var docs = value.docs.map((e) => e.data());
+  // addEvent(EventModel event) async{
+  //   CollectionReference events = FirebaseFirestore.instance.collection('events');
+  //   await events.doc(event.id).set(event.toJson(event)).whenComplete(() {
+  //     print("EVENT SUCCESSFULLY ADDED TO FIRESTORE");
+  //   }).catchError((e){
+  //     print(e);
+  //   });
+  // }
 
-      docs.forEach((element) {
-        GroupModel groupModel = GroupModel.fromJSON(element);
-        groupNames.add(groupModel.title);
-        });
-      });
+  // addGroup(GroupModel group) async{
+  //   CollectionReference groups = FirebaseFirestore.instance.collection('groups');
+  //   await groups.doc(group.title).set(toJSON(group)).whenComplete(() => {
+  //     print("GROUP ADDED TO FIREBASE SUCCESSFULLY")
+  //   });
+  // }
 
-      print(groupNames);
+  // getGroupsNameList(String name){
+  //   List<String> groupNames = [];
+  //   CollectionReference groups = FirebaseFirestore.instance.collection("groups");
+
+  //   var getDocs = groups.get().then((value){
+  //     var docs = value.docs.map((e) => e.data());
+
+  //     docs.forEach((element) {
+  //       GroupModel groupModel = GroupModel.fromJSON(element);
+  //       groupNames.add(groupModel.title);
+  //       });
+  //     });
+
+  //     print(groupNames);
+  // }
+
+  // getGroupsEmailList(String name){
+  //   List<String> groupEmails = [];
+  //   CollectionReference groups = FirebaseFirestore.instance.collection("groups");
+
+  //   var getDocs = groups.where("title",isEqualTo: name).get().then((value){
+  //     var docs = value.docs.map((e) => e.data());
+
+  //     docs.forEach((element) {
+  //       GroupModel groupModel = GroupModel.fromJSON(element);
+  //       groupModel.emailList.forEach((element) {
+  //         groupEmails.add(element);
+  //       });
+  //       });
+  //     print(groupEmails);
+  //    });
   }
-
-  getGroupsEmailList(String name){
-    List<String> groupEmails = [];
-    CollectionReference groups = FirebaseFirestore.instance.collection("groups");
-
-    var getDocs = groups.where("title",isEqualTo: name).get().then((value){
-      var docs = value.docs.map((e) => e.data());
-
-      docs.forEach((element) {
-        GroupModel groupModel = GroupModel.fromJSON(element);
-        groupModel.emailList.forEach((element) {
-          groupEmails.add(element);
-        });
-        });
-      print(groupEmails);
-     });
-  }
-}
