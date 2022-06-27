8b473d2b0b2a8f9046c0ecb71caac947272c4b76
akshay@96804 <19uec032@lnmiit.ac.in>
2022-06-27 15:55:22 +0530

    Navigation added

--- lib/screens/landingpage.dart
+++ lib/screens/landingpage.dart
@@ -1,100 +1,57 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:meet_up/screens/allEvents.dart';
+import 'package:meet_up/screens/HomePage.dart';
 import 'package:meet_up/screens/allGroups.dart';
-import 'package:meet_up/screens/createGroupScreen.dart';
 import 'package:meet_up/screens/createEventScreen.dart';
 
-class LandingPage extends StatelessWidget {
-  // const LandingPage({ Key? key }) : super(key: key);
+class LandingPage extends StatefulWidget {
+  @override
+  _LandingPageState createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  int _selectedIndex = 0;
+
+  List<Map<String, dynamic>> _pages = [
+    {'title': 'Home', 'screen': HomePage()},
+    {'title': 'Create', 'screen': CreateMeet()},
+    {'title': 'Groups', 'screen': GroupPage()}
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text('MeetUp',style: GoogleFonts.poppins(
-            fontSize: 30.0,
-            fontWeight: FontWeight.bold
-          ),),
-           Container(
-              height: MediaQuery.of(context).size.height*0.2,
-              child: SvgPicture.asset('assets/img.svg')
-            ),
-          Column(
-            children: [
-              GestureDetector(
-                onTap: (){
-                  Navigator.push(context, MaterialPageRoute(builder: (context){
-                    return createEvent(); 
-                  }));
-                },
-                child: Container(
-                  padding: EdgeInsets.all(12.0),
-            alignment: Alignment.center,
-            color: Colors.redAccent,
-            margin: EdgeInsets.symmetric(horizontal: 15.0,vertical: 5.0),
-            width: double.infinity,
-            child: Text('Create A New Event',style: GoogleFonts.poppins(
-                color: Colors.white
-            ),),
-          ),
-              ),
-          GestureDetector(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context){
-                return AllMeetsScreen(); 
-              }));
-            },
-            child: Container(
-              padding: EdgeInsets.all(12.0),
-              alignment: Alignment.center,
-              color: Colors.redAccent,
-              margin: EdgeInsets.symmetric(horizontal: 15.0,vertical: 5.0),
-              width: double.infinity,
-              child: Text('View All Events',style: GoogleFonts.poppins(
-                color: Colors.white
-              ),),
-            ),
-          ),
-          GestureDetector(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context){
-                return CreateGroup(); 
-              }));
-            },
-            child: Container(
-              padding: EdgeInsets.all(12.0),
-              alignment: Alignment.center,
-              color: Colors.redAccent,
-              margin: EdgeInsets.symmetric(horizontal: 15.0,vertical: 5.0),
-              width: double.infinity,
-              child: Text('Create A New Group',style: GoogleFonts.poppins(
-                color: Colors.white
-              ),),
-            ),
-          ),
-          GestureDetector(
-            onTap: (){
-               Navigator.push(context, MaterialPageRoute(builder: (context){
-                return AllGroupsScreen(); 
-              }));
-            },
-            child: Container(
-              padding: EdgeInsets.all(12.0),
-              alignment: Alignment.center,
-              color: Colors.redAccent,
-              margin: EdgeInsets.symmetric(horizontal: 15.0,vertical: 5.0),
-              width: double.infinity,
-              child: Text('View All Groups',style: GoogleFonts.poppins(
-                color: Colors.white
-              ),),
-            ),
-          )
-            ],
-          )
+      appBar: AppBar(
+        title: Text("Meetify"),
+        actions: [
+          IconButton(icon: Icon(Icons.logout), onPressed: (){
+            showDialog(context: context, builder: (context){
+              return AlertDialog(
+                title: Text("Are You Sure ?"),
+                actions: [
+                  MaterialButton(onPressed: (){
+
+                  },child: Text("Yes"),),
+                  MaterialButton(onPressed: (){
+                    Navigator.pop(context);
+                  },child: Text("No")),
+                ],
+              );
+            });
+          })
+        ],
+      ),
+      body: _pages[_selectedIndex]['screen'],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(title:Text('Home'),icon: Icon(Icons.home)),
+          BottomNavigationBarItem(title:Text('Create Meet'),icon: Icon(Icons.add)),
+          BottomNavigationBarItem(title:Text('Groups'),icon: Icon(Icons.group_add_sharp)),
         ],
       ),
     );
